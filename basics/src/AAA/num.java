2d156eccd074458660a02e94380ef025ccd7dfb3
HP <HP@LAPTOP-BM3788FD>
2022-01-21 18:50:19 +0530

    first commit

---
+++ basics/src/AAA/num.java
@@ -0,0 +1,17 @@
+package AAA;
+
+class num {
+	public static void main(String[] args)
+	{
+		int n=-5;
+		if(n>0)
+		{
+			System.out.println("number is positive");
+		}
+		else
+		{
+			System.out.println("number is negative");
+		}
+	}
+
+}
---
+++ basics/src/AAA/positive.java
@@ -0,0 +1,14 @@
+package AAA;
+
+class positive {
+	public static void main(String[] args) {
+		
+	
+	int x=9 , y=8;
+	int temp = x;
+	x=y;
+	y=temp;
+	System.out.println("x="+x);
+	System.out.println("y="+y);
+}
+}
